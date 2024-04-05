26cf91edebfbaceceb05aa5f77e0e6b1114c0dd6
HFwas <1558934404@qq.com>
2024-04-05 10:24:17 +0800

    add baidu,360,bing,google

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,6 +9,21 @@ export default defineUserConfig({
   title: "HFwas",
   description: "HFwas的博客",
 
+  head: [
+      ['meta', {name: 'baidu-site-verification', content: 'codeva-0ATRAupVYi'}],
+      ['meta', {name: '360-site-verification', content: 'bb63045cb42977fa44dba977f095a42f'}],
+      ['meta', {name: 'msvalidate.01', content: '8F660B701AFF402CFE968D8155346E6A'}],
+      ['meta', {name: "google-site-verification",content: "haDC5Fd6-XOF14hosBI6MvUXfI6ajLC68Dvct3IE3zU"}],
+      ['script', {}, `var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?8dc86dd1ce3336d553ac539d00ff3dcc";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();`
+      ],
+      ],
+
   theme,
 
   // 和 PWA 一起启用
