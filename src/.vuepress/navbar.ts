e70c8f0920e477d50333937581a428294f1a47ce
HFwas <1558934404@qq.com>
2025-04-21 13:15:57 +0800

    update navbar.ts

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,8 +1,8 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  { text: "Devops 技能", icon: "java", link: "/devops/" },
-  { text: "开发相关", icon: "github", link: "/dev/" },
-  { text: "语言知识", icon: "book", link: "/language/" },
+  { text: "Devops 技能", icon: "/icons8-devops-60.png", link: "/devops/" },
+  { text: "开发相关", icon: "/icons8-开发人员-50.png", link: "/dev/" },
+  { text: "语言知识", icon: "/icons8-语言-50.png", link: "/language/" },
   { text: "其他", icon: "book", link: "/other/" },
 ]);
